8d8aae015ee8282bd70b74890eca8835d0539b6c
Yulia V <yvoev@mail.ru>
2021-03-22 19:33:33 +0000

    changed create_canvas_and_axes

--- MY_zyxxy_SETTINGS.py
+++ MY_zyxxy_SETTINGS.py
@@ -70,13 +70,13 @@ my_default_font_sizes = {'title'      : 18/screen_zoom,
 # Figure size in pixels is DPI * figure size in inches
 
 my_default_display_params = {'max_figsize' : [8/screen_zoom, 7.5/screen_zoom],
-                             'dpi' : 50,
+                             'dpi' : 100,
                              'margin_adjustments' : {'top'    : 0.85, 
                                                      'bottom' : 0.05,
                                                      'left'   : 0.05,
                                                      'right'  : 0.95,
                                                      'ticks'  : 0.15}}
-my_default_image_params = {'dpi'     : 20,
+my_default_image_params = {'dpi'     : 50,
                            'format'  : 'png'}
 
 my_default_animation_params = {'dpi'     : 70,
--- zyxxy_canvas.py
+++ zyxxy_canvas.py
@@ -19,6 +19,7 @@ from functools import partial
 from math import floor
 from matplotlib import animation
 import matplotlib.pyplot as plt
+import numpy as np
 
 from zyxxy_shape_style import set_diamond_size_factor, set_outlines_colour, find_colour_code
 from zyxxy_shape_class import get_all_polygons_in_layers
@@ -26,58 +27,19 @@ from zyxxy_external_images import filename_to_image, show_image
 from zyxxy_utils import is_running_tests
 from MY_zyxxy_SETTINGS import my_default_font_sizes, my_default_display_params, my_default_image_params, my_default_animation_params
 
-# create the axis, set their sizes, 
-# add the grid and ticks if needed
-def create_canvas_and_axes(canvas_width,
-                           canvas_height,
-                           make_symmetric = False,
-                           tick_step = None,
-                           background_colour = 'none',
-                           title = None,
-                           title_font_size = my_default_font_sizes['title'],
-                           axes_label_font_size = my_default_font_sizes['axes_label'],
-                           axes_tick_font_size = my_default_font_sizes['tick'],
-                           max_figsize = my_default_display_params['max_figsize'],
-                           dpi = my_default_display_params['dpi'],
-                           default_margin_adjustments = my_default_display_params['margin_adjustments'],
-                           axes = None,
-                           model = None,
-                           outlines_colour = None):
+def __prepare_axes(ax, canvas_width,
+                       canvas_height, make_symmetric, axes_label_font_size, axes_tick_font_size, tick_step, background_colour):
 
-  margin_adjustments = {key:value for key, value in default_margin_adjustments.items() if key!='ticks'}
-  if tick_step is not None:
-    margin_adjustments['left']   += default_margin_adjustments['ticks']
-    margin_adjustments['bottom'] += default_margin_adjustments['ticks']
-
-  
-  scale_horizontal = (margin_adjustments['right'] - margin_adjustments['left']) * max_figsize[0] / canvas_width
-  scale_vertical = (margin_adjustments['top'] - margin_adjustments['bottom']) * max_figsize[1] / canvas_height
-  if model is not None:
-    # decide if the model should be below (vertical) or to the right (horizontal) of the working axes
-    scale_if_V_placement = min(scale_vertical/2, scale_horizontal)
-    scale_if_H_placement = min(scale_vertical, scale_horizontal/2)
-
-    place_model_H_not_V = (scale_if_H_placement > scale_if_V_placement)
-    figsize = max_figsize
-    if place_model_H_not_V:
-      figsize[1] *= scale_if_V_placement/scale_if_H_placement
-    else:
-      figsize[0] *= scale_if_H_placement/scale_if_V_placement
-  else:
-    figsize = max_figsize
-    if scale_horizontal < scale_vertical:
-      figsize[1] *= scale_horizontal/scale_vertical
-    else:
-      figsize[0] *= scale_vertical/scale_horizontal
+  assert make_symmetric in ['x', 'y', True, False]
 
   # helper function to make sure the ticks are in the right place
   def get_round_multiple_range(min_, max_, step):
+    if step is None:
+      return []
     _sign = -1 if min_ < 0 else 1
     min_multiple = _sign * floor(abs(min_/step)) * abs(step)
     result = np.arange(min_multiple, max_, step)
     return result
-
-  assert make_symmetric in ['x', 'y', True, False]
 
   if make_symmetric in ['x', True]:
     left_x, right_x = -canvas_width/2, canvas_width/2
@@ -88,79 +50,118 @@ def create_canvas_and_axes(canvas_width,
   else:
     bottom_y, top_y = 0, canvas_height
 
-  all_axes = []
-  if axes is None:
-    if model is None:
-      _, axes = plt.subplots()
-      all_axes.append(axes)
-    else:
-      if place_model_H_not_V:
-        _, axs = plt.subplots(1, 2)
-      else:
-        _, axs = plt.subplots(2, 1)
-      all_axes += [axs[0], axs[1]]
-      axes = axs[0]
-  else:
-    all_axes = [axes]
-
-  for a_ in all_axes:
-    a_.grid(tick_step is not None)
-    if tick_step is not None:
-      a_.set_xlabel("RULER FOR X's", fontsize=axes_label_font_size)
-      a_.set_ylabel("RULER FOR Y's", fontsize=axes_label_font_size)
-      a_.tick_params(axis='both', which='major', labelsize=axes_tick_font_size)
-      a_.set_xticks(ticks = get_round_multiple_range(left_x, right_x, tick_step))
-      a_.set_yticks(ticks = get_round_multiple_range(bottom_y, top_y, tick_step))
-    else:
-      a_.set_xticks(ticks = [])
-      a_.set_yticks(ticks = [])
-    # set axis limits
-    a_.set_aspect('equal')
-    a_.set_xlim(left=left_x, right=right_x)
-    a_.set_ylim(bottom=bottom_y, top=top_y)
-
+  ax.grid(tick_step is not None)
   if tick_step is None:
-    axes.set_facecolor(find_colour_code(background_colour))
+    ax.set_facecolor(find_colour_code(background_colour))
+  else:
+    ax.set_xlabel("RULER FOR X's", fontsize=axes_label_font_size)
+    ax.set_ylabel("RULER FOR Y's", fontsize=axes_label_font_size)
+    ax.tick_params(axis='both', which='major', labelsize=axes_tick_font_size)
+
+  ax.set_xticks(ticks = get_round_multiple_range(left_x, right_x, tick_step))
+  ax.set_yticks(ticks = get_round_multiple_range(bottom_y, top_y, tick_step))
+  # set axis limits
+  ax.set_xlim(left=left_x, right=right_x)
+  ax.set_ylim(bottom=bottom_y, top=top_y)
+  ax.set_aspect('equal')
+    
+
+# create the axis, set their sizes, 
+# add the grid and ticks if needed
+def create_canvas_and_axes(canvas_width,
+                           canvas_height,
+                           make_symmetric = False,
+                           tick_step = None,
+                           background_colour = 'none',
+                           axes_label_font_size = my_default_font_sizes['axes_label'],
+                           axes_tick_font_size = my_default_font_sizes['tick'],
+                           title = "",
+                           title_font_size = my_default_font_sizes['title'],
+                           max_figsize = my_default_display_params['max_figsize'],
+                           dpi = my_default_display_params['dpi'],
+                           default_margin_adjustments = my_default_display_params['margin_adjustments'],
+                           axes = None,
+                           model = None,
+                           outlines_colour = None):
+
+  params_for_axes = {  'canvas_width'         : canvas_width,
+                       'canvas_height'        : canvas_height, 
+                       'make_symmetric'       : make_symmetric,
+                       'tick_step'            : tick_step, 
+                       'background_colour'    : background_colour, 
+                       'axes_label_font_size' : axes_label_font_size, 
+                       'axes_tick_font_size'  : axes_tick_font_size}
+
+  if axes is not None:
+    __prepare_axes(ax=axes, **params_for_axes)
+    return axes
+
+  margin_adjustments = {key:value for key, value in default_margin_adjustments.items() if key!='ticks'}
+  if tick_step is not None:
+    margin_adjustments['left']   += default_margin_adjustments['ticks']
+    margin_adjustments['bottom'] += default_margin_adjustments['ticks']
+  
+  scale_horizontal = (margin_adjustments['right'] - margin_adjustments['left']) * max_figsize[0] / canvas_width
+  scale_vertical = (margin_adjustments['top'] - margin_adjustments['bottom']) * max_figsize[1] / canvas_height
+  figsize = max_figsize
+  if model is not None:
+      # decide if the model should be below (vertical) or to the right (horizontal) of the working axes
+      scale_if_V_placement = min(scale_vertical/2, scale_horizontal)
+      scale_if_H_placement = min(scale_vertical, scale_horizontal/2)
+
+      place_model_H_not_V = (scale_if_H_placement > scale_if_V_placement)
+      
+      if place_model_H_not_V:
+        figsize[1] *= scale_if_V_placement/scale_if_H_placement
+        margin_adjustments['left'] /= 2.
+        margin_adjustments['right'] = (1 + margin_adjustments['right']) / 2.
+      else:
+        figsize[0] *= scale_if_H_placement/scale_if_V_placement
+        margin_adjustments['bottom'] /= 2.
+        margin_adjustments['top'] = (1 + margin_adjustments['top']) / 2.
+
+  else:
+      if scale_horizontal < scale_vertical:
+        figsize[1] *= scale_horizontal / scale_vertical
+      else:
+        figsize[0] *= scale_vertical / scale_horizontal
+
+  spa = [] if model is None else ([1, 2] if place_model_H_not_V else [2, 1])
+  figure, axs = plt.subplots(*spa)
+  axes = axs if not isinstance(axs, np.ndarray) else axs[0]
+  
+  figure.subplots_adjust(**margin_adjustments)
+  figure.set_dpi(dpi) 
+  figure.set_size_inches(figsize)
+
+  __prepare_axes(ax=axes, **params_for_axes)
   
   if model is not None:
     # handle the model drawing
     if isinstance(model, str):
+      __prepare_axes(ax=axs[1], **params_for_axes)
       model_title = "Original Drawing"
       image = filename_to_image(filename=model)
       scaling_factor = min(canvas_width/image.shape[1], canvas_height/image.shape[0])
       # defining LB_position to center the model image
-      LB_position=[left_x   + 0.5 * (canvas_width  - image.shape[1] * scaling_factor), 
-                   bottom_y + 0.5 * (canvas_height - image.shape[0] * scaling_factor)]
+      LB_position=[axs[1].get_xticks()[0] + 0.5 * (canvas_width  - image.shape[1] * scaling_factor), 
+                   axs[1].get_yticks()[0] + 0.5 * (canvas_height - image.shape[0] * scaling_factor)]
       # placing the image
       show_image(ax=axs[1], prepared_image=image, origin=[0, 0], zorder=0, scaling_factor=scaling_factor,  LB_position=LB_position)  
     else:
       model(axes=axs[1]) 
       model_title = "Completed Drawing"
-
-    if place_model_H_not_V:   
-      margin_adjustments['left'] /= 2.
-      margin_adjustments['right'] = (1 + margin_adjustments['right']) / 2.
-    else:
-      margin_adjustments['bottom'] /= 2.
-      margin_adjustments['top'] = (1 + margin_adjustments['top']) / 2.
-
+      if outlines_colour is not None:
+        set_outlines_colour(outlines_colour)
+        set_diamond_size_factor(0)
+        model(axes=axes)
+        set_outlines_colour(None)
     axs[1].set_title(model_title, fontdict={'size': title_font_size})
-    if outlines_colour is not None and model is not None and not isinstance(model, str):
-      set_outlines_colour(outlines_colour)
-      set_diamond_size_factor(0)
-      model(axes=axes)
-      axes.set_title("") # remove the title if needed
-      set_outlines_colour(None)
 
   axes.set_title(title, fontdict={'size': title_font_size})
 
   # show diamond points and grid and axis if and only if tick_step is set
   set_diamond_size_factor(value=(tick_step is not None))
-
-  plt.subplots_adjust(**margin_adjustments)
-  figure = plt.gcf()
-  figure.set_dpi(dpi) 
-  figure.set_size_inches(figsize)
 
   return axes
 
@@ -197,7 +198,6 @@ def show_drawing_and_save_if_needed(filename=None,
                            nb_of_frames = None,
                            block=True):
   figure = plt.gcf()
-
   current_dpi = figure.get_dpi() 
 
   if (animation_func is None) != (nb_of_frames is None):
@@ -226,5 +226,6 @@ def show_drawing_and_save_if_needed(filename=None,
       anim.save("images_videos/"+filename+'.'+animation_format, writer=writer)
       return
   figure.set_dpi(current_dpi) 
+
   if not is_running_tests():
     plt.show(block=block)
