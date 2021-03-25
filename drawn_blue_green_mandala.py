eb41cd92c7443e8b216856165a827e2a0c2741fd
Yulia V <yvoev@mail.ru>
2021-03-25 18:02:03 +0000

    more work on masala

--- drawn_blue_green_mandala.py
+++ drawn_blue_green_mandala.py
@@ -1,8 +1,8 @@
 
 from zyxxy_canvas import create_canvas_and_axes, show_drawing_and_save_if_needed
-from zyxxy_shape_functions import draw_a_triangle, draw_a_rectangle, draw_a_star, draw_a_circle, draw_a_crescent, draw_an_ellipse, draw_an_egg, draw_a_segment
+from zyxxy_shape_functions import draw_a_triangle, draw_a_rectangle, draw_a_star, draw_a_circle, draw_a_crescent, draw_an_ellipse, draw_an_egg, draw_a_segment, draw_a_sector
 from zyxxy_shape_style import set_default_patch_style, set_default_line_style, set_default_outline_style, new_layer, new_layers_outline_behind
-from zyxxy_utils import tan_hours
+from zyxxy_utils import tan_hours, asin_hours, sin_hours, cos_hours
 
 #######################################################
 # Creating the canvas!                               ##
@@ -19,9 +19,13 @@ ax = create_canvas_and_axes(  canvas_width = 80,
 # Now let's draw the shapes!                         ##
 
 distance_crescents = 38
-distance_circles = 31
+distance_circles = 30
 distance_triangles = 27
+triangle_height = 8
 crescents_qty = 40
+arc_distance = 16
+arc_width = 1
+init_radius_arc = 6.5
 
 set_default_outline_style(linewidth=3)
 
@@ -40,18 +44,38 @@ for i in range(crescents_qty):
 layers_3 = new_layers_outline_behind()
 set_default_patch_style(colour="yellow")
 
-for i in range(1):#8):
-  triangles = [draw_a_triangle(ax=ax, tip_x=0, tip_y=distance_triangles, height=8, width=3.5, turn=6) for _ in range(4)]
+for i in range(8):
+  triangles = [draw_a_triangle(ax=ax, tip_x=0, tip_y=distance_triangles, height=triangle_height, width=3.5, turn=6) for _ in range(4)]
   for t, angle in enumerate([-0.19, -0.1, 0.1, 0.19]):
     triangles[t].rotate(turn=angle, diamond_override=[0,0])
 
-  big_triangles = [draw_a_triangle(ax=ax, tip_x=0, tip_y=distance_triangles, height=8, width=7, turn=6) for _ in range(4)]
+  big_triangles = [draw_a_triangle(ax=ax, tip_x=0, tip_y=distance_triangles, height=triangle_height, width=13, turn=6) for _ in range(2)]
   for t, lr in enumerate([-1, 1]):
-    print(lr*1.5)
-    big_triangles[t].rotate(turn=-lr*1.5)
-    big_triangles[t].rotate(turn=lr*1.5, diamond_override=[0,0])
+    big_triangles[t].rotate(turn=-lr*.1, diamond_override=[0, distance_triangles-triangle_height])
+    big_triangles[t].rotate(turn=lr*.5, diamond_override=[0,0])
 
+  for trngl in triangles + big_triangles:
+    trngl.rotate(turn=12/8*i, diamond_override=[0,0])
 
+layers_4 = new_layers_outline_behind()
+print(sin_hours(12/16) * arc_distance, init_radius_arc)
+assert sin_hours(12/16) * arc_distance < init_radius_arc
+angle_one_arc = asin_hours(sin_hours(12/16) * arc_distance / init_radius_arc)
+arc_colours = ['royalblue', 'powderblue']
+centre_arc_y = arc_distance * cos_hours(12/16) - init_radius_arc * cos_hours(angle_one_arc)
+for i in range(8):
+  sectors = [draw_a_sector(ax=ax, 
+                           angle_start=-angle_one_arc, 
+                           angle_end=angle_one_arc, 
+                           radius=init_radius_arc + r * arc_width, 
+                           radius_2=init_radius_arc + (r+1) * arc_width, 
+                           centre_x=0, 
+                           centre_y=centre_arc_y,
+                           colour=arc_colours[r]) for r in range(2)]
+  for s in sectors:
+    s.rotate(turn=12/8*i, diamond_override=[0,0])
+
+  draw_a_segment(ax=ax, start_x=0, start_y=0, turn=12/8*i, length=centre_arc_y+init_radius_arc)
 
 
 show_drawing_and_save_if_needed()
